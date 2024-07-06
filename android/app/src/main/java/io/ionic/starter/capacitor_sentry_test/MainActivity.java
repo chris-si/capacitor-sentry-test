aefda60aabb93bcfeae48612608179b7b11e81cd
Chris/si <24972677+chris-si@users.noreply.github.com>
2024-07-06 10:54:04 +0200

    add android

---
+++ android/app/src/main/java/io/ionic/starter/capacitor_sentry_test/MainActivity.java
@@ -0,0 +1,19 @@
+package io.ionic.starter.capacitor_sentry_test;
+
+import android.os.Bundle;
+import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.Plugin;
+import io.sentry.capacitor.SentryCapacitor;
+import java.util.ArrayList;
+
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    // Initializes the Bridge
+    this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
+      // sentry-capacitor plugin
+      add(SentryCapacitor.class);
+    }});
+  }
+}
